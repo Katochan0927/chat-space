b16e08f957d0dfbf9dc77c7b8a08513234f037b4
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-07-10 17:17:04 +0900

    Add column in table of messages

--- db/migrate/20190710074648_create_messages.rb
+++ db/migrate/20190710074648_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text        :body       #bodyカラム作成
+      t.string      :image      #imageカラム作成
+      t.integer     :group_id   #group_idカラム作成
+      t.integer     :user_id    #user_idカラム作成
       t.timestamps
     end
   end
